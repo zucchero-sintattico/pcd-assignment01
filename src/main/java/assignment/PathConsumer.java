6aee009edb33da98b08ee559aed5ce433bdb8060
Alessandro Mazzoli <alessandro.mazzoli9@studio.unibo.it>
2023-04-04 16:33:17 +0200

    🐛 fix(PathConsumer.java): handle IOException when reading file ✨ feat(PathConsumer.java): add Barrier to synchronize threads on queue close

--- src/main/java/assignment/PathConsumer.java
+++ src/main/java/assignment/PathConsumer.java
@@ -1,27 +1,40 @@
 package assignment;
 
 import lib.architecture.QueueConsumerThread;
+import lib.synchronization.Barrier;
 import lib.synchronization.QueueMonitor;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class PathConsumer extends QueueConsumerThread<Path> {
-    private QueueMonitor<Statistic> statsQueue;
-    public PathConsumer(QueueMonitor<Path> queueMonitor, QueueMonitor<Statistic> statsQueue) {
+    private final QueueMonitor<Statistic> statsQueue;
+    private final Barrier barrier;
+
+    public PathConsumer(final QueueMonitor<Path> queueMonitor, final QueueMonitor<Statistic> statsQueue, final Barrier barrier) {
         super(queueMonitor);
         this.statsQueue = statsQueue;
+        this.barrier = barrier;
     }
 
     @Override
     public void consume(Path filepath) {
-        int lines = Files.readAllLines(filepath).size();
-        System.out.println(filepath + " has " + lines + " lines");
-        this.statsQueue.enqueue(new Statistic(filepath, lines));;
+        try {
+            final int lines = Files.readAllLines(filepath).size();
+            System.out.println(filepath + " has " + lines + " lines");
+            this.statsQueue.enqueue(new Statistic(filepath, lines));;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public void onQueueClosed() {
-        this.statsQueue.close();
+        try {
+            this.barrier.hitAndWaitAll();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
