2cefff081ed5bdf7c9868efcb3e5be52e31cb342
Alessandro Mazzoli <alessandro.mazzoli9@studio.unibo.it>
2023-04-04 16:13:10 +0200

    🚧 chore(Main.java): refactor code to use ActionBarrier class 🚧 chore(ActionBarrier.java): refactor code to use Monitor class 🚧 test(FooTest.java): add test for ActionBarrier class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,32 @@
+import gov.nasa.jpf.vm.Verify;
+import lib.synchronization.ActionBarrier;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        final ActionBarrier barrier = new ActionBarrier(2, () -> {
+            System.out.println("Barrier completed");
+        });
+        final Set<Thread> threads = new HashSet<>();
+        for (int i = 0; i < 2; i++) {
+            threads.add(new Thread(() -> {
+                System.out.println("Hitting barrier");
+                barrier.hitAndWaitAll();
+            }));
+        }
+        threads.forEach(Thread::start);
+        for (Thread thread : threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
 
--- src/test/java/FooTest.java
+++ src/test/java/FooTest.java
@@ -1,10 +1,16 @@
+import lib.synchronization.ActionBarrier;
 import org.junit.jupiter.api.Test;
 
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class FooTest {
     @Test
-    public void testFoo() {
-        assertTrue(true);
+    public void testActionBarrier() {
+
     }
 }
