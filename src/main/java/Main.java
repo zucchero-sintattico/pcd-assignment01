e17f8906228e074a27ef7704643cb1bc5e82ff22
Alessandro Mazzoli <alessandro.mazzoli9@studio.unibo.it>
2023-04-04 16:15:46 +0200

    🚧 chore(Main.java): remove unused code ✅ test(FooTest.java): add test for ActionBarrier class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,24 +9,7 @@ import java.util.stream.IntStream;
 
 public class Main {
     public static void main(String[] args) {
-        final ActionBarrier barrier = new ActionBarrier(2, () -> {
-            System.out.println("Barrier completed");
-        });
-        final Set<Thread> threads = new HashSet<>();
-        for (int i = 0; i < 2; i++) {
-            threads.add(new Thread(() -> {
-                System.out.println("Hitting barrier");
-                barrier.hitAndWaitAll();
-            }));
-        }
-        threads.forEach(Thread::start);
-        for (Thread thread : threads) {
-            try {
-                thread.join();
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-        }
+
     }
 }
 
--- src/test/java/FooTest.java
+++ src/test/java/FooTest.java
@@ -1,6 +1,7 @@
 import lib.synchronization.ActionBarrier;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -11,6 +12,23 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class FooTest {
     @Test
     public void testActionBarrier() {
-
+        final ActionBarrier barrier = new ActionBarrier(2, () -> {
+            assertTrue(true);
+        });
+        final Set<Thread> threads = new HashSet<>();
+        for (int i = 0; i < 2; i++) {
+            threads.add(new Thread(() -> {
+                System.out.println("Hitting barrier");
+                barrier.hitAndWaitAll();
+            }));
+        }
+        threads.forEach(Thread::start);
+        for (Thread thread : threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
