70b7dd4d37eb19ce95c0b0631751423df45ad480
luigi-borriello00 <luigi.borriello2@studio.unibo.it>
2023-04-04 15:15:10 +0200

    push Barrier

---
+++ src/main/java/assignment/BarrierImpl.java
@@ -0,0 +1,24 @@
+package pcd.lab04.monitors.barrier;
+
+public class BarrierImpl implements Barrier{
+    private int nArrivedSoFar;
+    private int nTotal;
+
+    public BarrierImpl(int nTotal){
+        this.nTotal = nTotal;
+        this.nArrivedSoFar = 0;
+    }
+
+    @Override
+    public synchronized void hitAndWaitAll() throws InterruptedException {
+        this.nArrivedSoFar++;
+        if(this.nArrivedSoFar == this.nTotal){
+            notifyAll();
+        }
+        else{
+            while (this.nArrivedSoFar < this.nTotal){
+                wait();
+            }
+        }
+    }
+}
---
+++ src/main/java/assignment/FileAnalyzer.java
@@ -0,0 +1,14 @@
+package assignment;
+
+import lib.architecture.QueueProducer;
+
+import java.nio.file.Path;
+
+public class FileAnalyzer implements QueueProducer<Path> {
+
+
+    @Override
+    public void run() {
+        File.
+    }
+}
