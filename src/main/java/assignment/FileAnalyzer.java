fac20230d35059242fd03ca49db6b3d3711f6615
luigi-borriello00 <luigi.borriello2@studio.unibo.it>
2023-04-04 15:24:09 +0200

    start analyzer

--- src/main/java/assignment/FileAnalyzer.java
+++ src/main/java/assignment/FileAnalyzer.java
@@ -1,14 +1,35 @@
 package assignment;
 
-import lib.architecture.QueueProducer;
+import lib.architecture.QueueProducerThread;
+import lib.synchronization.QueueMonitor;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 
-public class FileAnalyzer implements QueueProducer<Path> {
+public class FileAnalyzer extends QueueProducerThread<Path> {
 
+
+    public FileAnalyzer(QueueMonitor<Path> queue, Path path) {
+        super(queue, path);
+    }
+
+    @Override
+    public void produce(Path value) {
+        super.produce(value);
+    }
 
     @Override
     public void run() {
-        File.
+        try {
+            Files.walk(this.path).filter(Files::isRegularFile)
+                    .filter(p -> p.toString().endsWith(".java"))
+                    .forEach(f -> {
+                        produce(f);
+                    });
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
+
--- src/main/java/lib/architecture/QueueProducerThread.java
+++ src/main/java/lib/architecture/QueueProducerThread.java
@@ -2,10 +2,15 @@ package lib.architecture;
 
 import lib.synchronization.QueueMonitor;
 
+import java.nio.file.Path;
+
 public abstract class QueueProducerThread<T> extends Thread implements Producer<T> {
     private final QueueMonitor<T> queue;
-    public QueueProducerThread(final QueueMonitor<T> queue) {
+    protected final Path path;
+
+    public QueueProducerThread(final QueueMonitor<T> queue, final Path path) {
         this.queue = queue;
+        this.path = path;
     }
 
     @Override
