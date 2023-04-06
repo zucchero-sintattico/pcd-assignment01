fabc9b5a8b7269ab5da63f70fbfff0e04c75219a
Alessandro Mazzoli <alessandro.mazzoli9@studio.unibo.it>
2023-04-06 15:46:23 +0200

    ✨ feat(StatisticConsumer.java): add StatisticConsumer class to consume StatisticQueue values

---
+++ src/main/java/assignment/StatisticConsumer.java
@@ -0,0 +1,23 @@
+package assignment;
+
+import assignment.queue.StatisticQueue;
+import lib.architecture.QueueConsumer;
+import lib.architecture.QueueConsumerThread;
+
+public class StatisticConsumer extends QueueConsumerThread<Statistic> {
+
+    public StatisticConsumer(final StatisticQueue queue) {
+        super(queue);
+    }
+
+    @Override
+    public void consume(Statistic value) {
+        System.out.println("Consuming: " + value);
+    }
+
+    @Override
+    public void onQueueClosed() {
+        System.out.println("Queue closed");
+    }
+    
+}
